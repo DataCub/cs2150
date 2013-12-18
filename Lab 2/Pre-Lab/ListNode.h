05064a8920f6901b02e2e3419b3a1c79b95e2118
Robert Alexander <Robert@Roberts-MacBook-Pro-3.local>
2013-12-18 10:15:31 -0500

    Cleaned List Node Header file

--- Lab 2/Pre-Lab/ListNode.h
+++ Lab 2/Pre-Lab/ListNode.h
@@ -7,7 +7,6 @@
 #ifndef LISTNODE_H
 #define LISTNODE_H
 
-// needed because NULL is part of std namespace
 #include <iostream>
 using namespace std;
 
@@ -19,12 +18,7 @@ private:
     int value;
     ListNode *next, *previous;	//for doubly linked lists
     friend class List;	// List needs to be able to access/change
-    // ListNode's next and previous pointers
     friend class ListItr;	// ListItr needs to access/change
-
-    // Not writing a destructor is fine in this case since there is no
-    // dynamically allocated memory in this class
 
 };
 #endif
-/* end of ListNode.h listing */
